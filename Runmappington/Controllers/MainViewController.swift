b6fa5224efe9afde2cf286c4e3c0b60140bb37de
green6erry <green6erry@gmail.com>
2018-05-19 18:36:27 -0400

    initial commit

---
+++ Runmappington/Controllers/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Runmappington
+//
+//  Created by Amanda Brown on 5/19/18.
+//  Copyright © 2018 AGB Design. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
